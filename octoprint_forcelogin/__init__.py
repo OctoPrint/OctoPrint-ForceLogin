ab5ff55f9f6d63b94867900d749ac074a8829467
Gina Häußge <osd@foosel.net>
2016-06-24 14:29:57 +0200

    Track our files for cache validation, disable preemptive caching

--- octoprint_forcelogin/__init__.py
+++ octoprint_forcelogin/__init__.py
@@ -23,5 +23,21 @@ class ForceLoginPlugin(octoprint.plugin.UiPlugin,
 		from flask import render_template, make_response
 		return make_response(render_template("forcelogin_index.jinja2", **render_kwargs))
 
+	def get_ui_custom_tracked_files(self):
+		from os.path import join as opj
+
+		paths = [opj("static", "css", "forcelogin.css"),
+		         opj("static", "js", "main.js"),
+		         opj("static", "js", "viewmodel.js"),
+		         opj("static", "less", "forcelogin.less"),
+		         opj("templates", "parts", "forcelogin_css.jinja2"),
+		         opj("templates", "parts", "forcelogin_javascripts.jinja2"),
+		         opj("templates", "forcelogin_index.jinja2")]
+
+		return [opj(self._basefolder, path) for path in paths]
+
+	def get_ui_preemptive_caching_enabled(self):
+		return False
+
 __plugin_name__ = "ForceLogin"
 __plugin_implementation__ = ForceLoginPlugin()
